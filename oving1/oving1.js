813e2b676733e45d8ee9d0d4226ae450e630b0fb
Andreas Tolnes <andreas.tolnes@ymail.com>
2019-08-26 12:36:46 +0200

    Clean up

--- oving1/oving1.js
+++ oving1/oving1.js
@@ -1,10 +1,8 @@
 const a = [-1,3,-9,2,2,-1,2,-1,-5]
 function kurs(array){
-
     let buyDate = 0; // 1
 	let sellDate = 0; // 1
 	let sum = 0; // 1
-
 	// Loops through the days to buy stocks
 	for(let i = 0; i < array.length; i++){ // 1 + 2n
 		let temp = 0 // 1
@@ -20,19 +18,15 @@ function kurs(array){
 	}
 	return `Buy: ${buyDate}, sell: ${sellDate}, profit: ${sum}`
 } // Total time complexity n^2
-
 let randomArray = num =>{
 	return Array.from({length: num}, () => Math.floor(Math.random() * 10) - 5);
 }
-
 console.log(kurs(a));
-
 function time(param){
 	let start = new Date();
 	let runder = 0;
 	let tid;
 	let slutt;
-
 	do{
 		kurs(param);	
 		slutt = new Date();
@@ -40,7 +34,5 @@ function time(param){
 	} while (slutt.getTime() - start.getTime() < 1000);
 	tid = (slutt.getTime() - start.getTime()) / runder;
 	return `Millisekund pr. runde: ${tid}`
-
-
 }
 console.log(time(randomArray(10000)));
