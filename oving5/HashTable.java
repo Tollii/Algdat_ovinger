f5deb4e0c4bd3b6a7c6cf218e0d1533722d86c46
Tollii <andreas.tolnes@ymail.com>
2019-09-19 12:50:16 +0200

    wow

--- oving5/HashTable.java
+++ oving5/HashTable.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Node {
   private Node next;
   private String data;
 
   public Node(String data){
     this.data = data;
+  }
+
+  public Node next(){
+    return next;
+  }
+
+  public void setNext(Node next){
+    this.next = next;
+  }
+
+  public String getData(){
+    return data;
   }
 
 }
@@ -16,37 +32,40 @@ public class HashTable {
     Node entry = new Node(data);
 
     if(hashTable[index] != null){
-      if(hashTable[index].next == null){
-          hashTable[index].next = entry;
-      } else {
-        Node n1 = hashTable[index];
-
-        while(n1.next != null){
-          n1 = n1.next;
+      System.out.println("Collision! at #" + index);
+      if(hashTable[index].next() != null){
+        System.out.println("Collision again!");
+        Node n1 = hashTable[index].next();
+        int counter = 1;
+        while(n1.next() != null){
+          n1 = n1.next();
+          counter++;
         }
-        n1.next = entry;
+        System.out.println("Index #" + index + " linked list length: " + counter);
+        n1.setNext(entry);
+      } else {
+        hashTable[index].setNext(entry);
       }
     } else {
       hashTable[index] = entry;
     }
-    System.out.println("Index #" + index);
   }
 
   public int lookUp(String navn){
-
+    return -1;
   }
 
  // Finds hash by adding together the ascii values of the characters in the string, then dividing by 65
-  private int divHash(String str){
+  public int divHash(String str){
     int hash = 0;
     char[] letters  = str.toCharArray();
     for(int i = 0; i < letters.length; i++){
-      hash += ((int) letters[i]) * i;
+      hash += ((int) letters[i]) * i - ((int) 'a');
     }
     return hash % hashTable.length;
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException{
     HashTable table = new HashTable();
     String str1 = "abcd";
     String str2 = "cbda";
@@ -55,6 +74,16 @@ public class HashTable {
     table.addToTable(str1);
     table.addToTable(str2);
     table.addToTable(str3);
+
+    FileReader fr = new FileReader("navn.txt");
+    BufferedReader reader = new BufferedReader(fr);
+    String read = "";
+
+    while((read = reader.readLine()) != null){
+      table.addToTable(read);
+    }
+
+
   }
 
 
